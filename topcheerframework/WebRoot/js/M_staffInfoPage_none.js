e1992fc397c978e70ba5253d6292c8cb6ac25c66
maqr <maqr@b444c54d-ba6d-49cc-beb2-2835a8c8254e>
2016-11-18 07:19:59 +0000

    问卷一问卷二员工信息管理页面html.js,css

---
+++ topcheerframework/WebRoot/js/M_staffInfoPage_none.js
@@ -0,0 +1,10 @@
+function none(){
+	//var login,bgDiv;
+	document.getElementById("loginBox").style.display='none';
+	document.getElementById("bgDiv").style.display='none';
+	
+	//login.style.display='none';
+	//bgDiv.style.display='none';
+	
+	
+}
