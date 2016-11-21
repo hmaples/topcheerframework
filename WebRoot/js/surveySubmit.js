ac228a402daa0e1a445189fe4183afc467be8296
hmaple <hmaple@b444c54d-ba6d-49cc-beb2-2835a8c8254e>
2016-11-21 14:41:35 +0000

    事务示例

---
+++ WebRoot/js/surveySubmit.js
@@ -0,0 +1,43 @@
+function dataHandling(){
+
+	var radioValue = null;
+	var checkValue = null;
+	var textValue = null;
+	var question_answer;
+	for (var i = 0; i < $('#questionnaire').find("input[type='radio']").length; i++) {
+		if ($('#questionnaire').find("input[type='radio']")[i].checked) {
+			var radioValueTemp = $('#questionnaire').find("input[type='radio']")[i].value;
+			alert(radioValueTemp);
+		}}
+//			if (radioValue == null) {
+//				radioValue = radioValueTemp;
+//			} else {
+//				radioValue = radioValue + "," + radioValueTemp;
+//			}
+//		}
+//	}
+//	for (var j = 0; j < $('#surveyForm').find("input[type='checkbox']").length; j++) {
+//		if ($('#surveyForm').find("input[type='checkbox']")[j].checked) {
+//			var checkValueTemp = $('#surveyForm').find("input[type='checkbox']")[j].id;
+//			if (checkValue == null) {
+//				checkValue = checkValueTemp;
+//			} else {
+//				checkValue = checkValue + "," + checkValueTemp;
+//			}
+//		}
+//	}
+//	for (var k = 0; k < $('#surveyForm').find("textarea").length; k++) {
+//		var textValueTemp =$('#surveyForm').find("textarea")[k].id+"_"+$('#surveyForm').find("textarea")[k].value;
+//		if (textValue == null) {
+//			textValue = textValueTemp;
+//		} else {
+//			textValue = textValue + "^" + textValueTemp;
+//		}
+//	}
+//	question_answer=radioValue+checkValue+textValue;
+//	alert(question_answer);
+//	$('#radioValue').attr("value",radioValue);
+//	$('#checkValue').attr("value",checkValue);
+//	$('#textValue').attr("value",textValue);
+//	$('#surveyForm').submit();
+}
