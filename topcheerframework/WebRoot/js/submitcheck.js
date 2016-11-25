18cb96ce909679d3797ee353490d9c4082170d8d
shencg <shencg@b444c54d-ba6d-49cc-beb2-2835a8c8254e>
2016-11-25 06:35:06 +0000

    文本框自动勾选checkbox的方法从mouseleave改为blur(失去焦点)

--- topcheerframework/WebRoot/js/submitcheck.js
+++ topcheerframework/WebRoot/js/submitcheck.js
@@ -14,7 +14,7 @@ $(function(){
 
 //填写"其他"一栏的文字框后自动勾选"其他"复选框
 $(function(){
-	$("#else_1").mouseleave(function(){
+	$("#else_1").blur(function(){
 		if(document.getElementById('else_1').value !== '') {
 			$("#3_1_0").prop("checked",true);
 		}
@@ -23,7 +23,7 @@ $(function(){
 		}
 	})
 	
-		$("#else_2").mouseleave(function(){
+		$("#else_2").blur(function(){
 		if(document.getElementById('else_2').value !== '') {
 			$("#3_2_0").prop("checked",true);
 		}
